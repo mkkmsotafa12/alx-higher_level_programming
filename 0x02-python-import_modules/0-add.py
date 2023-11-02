d419b635bbbf9147fc9f4f265573749a5ba73d14
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-11-02 04:29:17 -0700

    add modifcation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
     from add_0 import add
-
-    # assign the value 1 to a variable called 'a'
     a = 1
-
-    # assign the value 2 to a variable called 'b'
     b = 2
-
-    # call the 'add' function from the 'add_0' module with 'a' and 'b' as arguments
     result = add(a, b)
-
-    # print the formatted string showing the addtion result
     print("{} + {} = {}".format(a, b, result))
