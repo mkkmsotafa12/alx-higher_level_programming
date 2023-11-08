1bb8b8bbf4255f13439a0993624abb149250bb39
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-11-08 08:33:38 -0800

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-        if my_list is None:
+    if my_list is None:
             return (None)
-        if len(my_list) == 0:
+    if len(my_list) == 0:
             return ([])
-        for c in my_list:
-            if c == search:
+    for c in my_list:
+        if c == search:
             new_list.append(replace)
         else:
             new_list.append(c)
-            return (new_list)
+    return (new_list)
 
