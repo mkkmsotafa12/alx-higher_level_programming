9158919c59da1d5764e30802f1ff29c3f23fa336
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-11-08 08:02:21 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if len(matrix) == 0 or matrix is None:
+        return None
+    new_matrix = []
+    for each in matrix:
+        new_matrix += ([x ** 2 for x in each])
+        return new_matrix
+
+
