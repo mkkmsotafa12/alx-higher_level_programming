11208b2c97eafb81d463ee0d6635b7eca96bacc4
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-11-08 08:37:23 -0800

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """search a list and replace it with replace
-
-    Args:
-        my_list: the list to search through
-        search: the element to search for
-        replace: the elemnt to replace it
-
-    Returns:
-        the new list with replace element
-    """
     new_list = []
     if my_list is None:
             return (None)
