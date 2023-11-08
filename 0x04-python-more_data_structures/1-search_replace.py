598dda714d085e707edcc005285a6814f4cc61b9
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-11-08 08:24:32 -0800

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy_list = []
+    new_list = []
     for i in range(len(my_list)):
         if my_list[i] == search:
-            copy_list.append(replace)
+            new_list.append(replace)
         else:
-            copy_list.append(my_list[i])
-            return (copy_list)
+            new_list.append(my_list[i])
+            return (new_list)
 
