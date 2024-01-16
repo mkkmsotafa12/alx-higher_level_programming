908b649ed883fa7f24c501b11483075c3282c4f2
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-01-16 11:42:35 -0800

    modify

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 
-ELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
