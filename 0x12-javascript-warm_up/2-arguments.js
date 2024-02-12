22e4927e53ca993464a90b3b1ad9a5993da5fc3e
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-02-12 06:16:43 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv.length == 3) {
-	console.log('Argument found')
+if (process.argv.length < 3) {
+	console.log('No argument')
 }
 else if (process.argv.length > 3) {
 	console.log("Argument found")
 }
 else {
-	console.log(`No argument`)
+	console.log(`Argument found`)
 }
 
