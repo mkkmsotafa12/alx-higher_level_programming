1e8da6d97f56bb20cd151a46b7b19c360a463cbc
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-02-13 12:17:37 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
-const Sq = require('./5-square.js');
-class Square extends Sq {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    let val = '';
-    for (let i = 0; i < this.width; i++) {
-      if (c == null) {
-        val += 'X'
-      } else {
-        val += c
-      }
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(val)
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) 
+	   console.log(c.repeat(this.width));
     }
   }
-}
-module.exports = val
+};
